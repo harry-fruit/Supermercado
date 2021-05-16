16685bdc0922bcd528602357217ee31541da4fd6
harry-fruit <63006188+harry-fruit@users.noreply.github.com>
2021-05-16 17:01:56 -0300

    Revert "Att"

--- client/src/app/components/contato.jsx
+++ client/src/app/components/contato.jsx
@@ -1,33 +1,10 @@
-import React, { Component, Fragment } from "react"
+import React, { Component } from "react"
 
 export class Contato extends Component{
     render(){
 
         return (
-            <>  
-               <h1>Contato</h1>
-               <section className="container">
-                  <section className="contato">  
-                    <h2>Esta com alguma Dúvida? Preencha o formulário para entrar em contato. </h2>
-                        <label>
-        
-                            <input type="text" placeholder="Nome"/>
-
-                            <input type="text" placeholder="Email" />
-                    
-                    
-                        </label>
-                        <label>
-        
-                            <textarea rows="4" cols="50"  placeholder="Digite sua Dúvida"></textarea>
-
-                    
-                        </label>
-                <button>Enviar</button>
-                  </section>
-                </section>
-            </>
-         
+            <h1>Contato</h1>
         )
 
     }
